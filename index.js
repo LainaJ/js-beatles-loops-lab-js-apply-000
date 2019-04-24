56c50a999f689612f9656ffcb76258ded22f8f6f
Laina Karosic <lainakarosic@gmail.com>
2019-04-24 22:37:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
 function theBeatlesPlay (musicians, instruments){
   for (var i = 0; i < musicians.length; i++)
   return i;
-  for (var j = 0; j < instruments.length; j++)
-  return j;
   }
 
   //var musicians = []
