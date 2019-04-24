97beda22932ca9db1e29f120f64f5cd6ac9eeb60
Laina Karosic <lainakarosic@gmail.com>
2019-04-24 22:31:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 
 
 function theBeatlesPlay (musicians, instruments){
-  var musicians = []
-  var instruments = []
-  musicians.push("John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr")
-  instruments.push("Guitar", "Bass Guitar", "Lead Guitar", "Drums")
   for (var i = 0; i < musicians.length; i++)
   return i + [" plays"];
   for (var j = 0; j < instruments.length; j++)
