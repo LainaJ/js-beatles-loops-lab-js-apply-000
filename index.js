bc5dd6ce6b1d3d14f32113f72b1dc4c63271a3da
Laina Karosic <lainakarosic@gmail.com>
2019-04-24 22:51:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,5 @@
 
 function theBeatlesPlay (musicians, instruments){
   for (var i = 0; i < musicians.length; i++)
-  return i;
+  return musicians + instruments;
   }
-
-  //var musicians = []
-  //var instruments = []
-  //musicians.push("John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr")
-  //instruments.push("Guitar", "Bass Guitar", "Lead Guitar", "Drums")
