171a3205ebe3a2fd79e33db94b5fa1aaafc9b201
Laina Karosic <lainakarosic@gmail.com>
2019-04-24 22:51:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //1) Beatles Loops returns an array of strings containing what instruments each musician plays
 
 
-function theBeatlesPlay (musicians, instruments){
+function theBeatlesPlay (musicians, instruments) {
   for (var i = 0; i < musicians.length; i++)
   return i;
   }
