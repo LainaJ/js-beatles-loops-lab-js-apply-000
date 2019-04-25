1d8647ba3dbc4a094820ed8100b11157cca9f5d1
Laina Karosic <lainakarosic@gmail.com>
2019-04-25 01:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function theBeatlesPlay (musicians, instruments) {
 //2)
 function johnLennonFacts() {
   var i = 0
-  var factsArray = [ "He was the last Beatle to learn to drive",
-      +  "He was never a vegetarian",
-      +  "He was a choir boy and boy scout",
-      +  "He hated the sound of his own voice"];
+  var factsArray = [`${He was the last Beatle to learn to drive},
+      +  ${He was never a vegetarian},
+      +  ${He was a choir boy and boy scout},
+      +  ${He hated the sound of his own voice}`];
 while (i < 4) {
   factsArray [i] += "!!!"; i++;
 }
