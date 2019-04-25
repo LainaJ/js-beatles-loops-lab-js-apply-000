2533a48619b64a90cc8715bc3daf6e623ab5d2eb
Laina Karosic <lainakarosic@gmail.com>
2019-04-25 00:05:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here//
 //1) Beatles Loops returns an array of strings containing what instruments each musician plays
 
-
 function theBeatlesPlay (musicians, instruments) {
+  emptyArray = []
   for (var i = 0; i < musicians.length; i++) {
-  return i;
+  emptyArray.push(i)
   }
 }
